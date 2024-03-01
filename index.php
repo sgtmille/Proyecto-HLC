d6e7d99de2c6534906488dde54a2820223b59408
Dumblarghost <unknow>
2024-03-01 04:06:36 -0600

    asdf

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo "Probando";
+require "vendor/autoload.php";
+use Pecee\SimpleRouter\SimpleRouter as Router;
+use Pecee\Http\Request;
+include "backend/clases/db.php";
+include "backend/includes/functions.php";
+
+$db = new db("hlc");
+
+Router::get("/",function(){
+  echo "Probando los cambios";
+});
